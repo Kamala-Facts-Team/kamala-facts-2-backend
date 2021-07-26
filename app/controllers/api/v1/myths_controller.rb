24b0d7ab819c118650febf4cfb1202535f5f703f
sydnee510 <sbsydnee161416@gmail.com>
2021-07-26 00:59:25 -0700

    build out myths controller

--- app/controllers/api/v1/myths_controller.rb
+++ app/controllers/api/v1/myths_controller.rb
@@ -2,5 +2,35 @@ class Api::V1::MythsController < ApplicationController
     def index
         @myths = Myth.all
         render json: @myths
+    end 
+
+    def create 
+        @myth = Myth.new(myth_params)
+        if @myth.save 
+            render json: @myth
+        else
+            render json: {error: 'Error: missing content'}
+        end
+    end 
+
+    def show 
+        @myth = Myth.find(params[:id])
+        render json: @myth
+    end
+
+    def destroy 
+        @myth = Myth.find(params[:id])
+        if @myth.destroy
+        render json: @myth.to_json
+        else 
+            render json: {error: "error deleting myth"}
+        end
+
+    end 
+
+    private
+
+    def myth_params 
+        params.require(:myth).permit(:lie_statement)
     end
 end
