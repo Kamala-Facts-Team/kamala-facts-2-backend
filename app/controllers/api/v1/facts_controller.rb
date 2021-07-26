fef7d9b851d8268d28c43e10ad747b7db88946e5
sydnee510 <sbsydnee161416@gmail.com>
2021-07-26 01:00:17 -0700

    build out facts controller with relations to each myth

--- app/controllers/api/v1/facts_controller.rb
+++ app/controllers/api/v1/facts_controller.rb
@@ -1,6 +1,32 @@
 class Api::V1::FactsController < ApplicationController
+    before_action :set_myth
+   
     def index
-        @facts = Fact.all
+            @facts = Fact.all 
         render json: @facts
+    end
+
+    def create 
+        @fact = @myth.facts.new(fact_params)
+        if @fact.save 
+            render json: @myth
+        else
+            render json: {error: 'Error: Need content and an url'}
+        end
+    end 
+
+    def show 
+        @fact = Fact.find(params[:id])
+        render json: @fact
+    end
+
+    private
+
+    def set_myth
+        @myth = Myth.find(params[:myth_id])
+    end 
+
+    def fact_params 
+        params.require(:fact).permit(:truth_statement, :receipt_source, :myth_id)
     end
 end
